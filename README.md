def146c9431e7773db97941f0d36994ca9b5d698
Richard Lindner <rlindner81@gmail.com>
2019-11-15 21:36:48 +0100

    Example yo

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# pushpull
+# PushPull
 Convenience tool to make your files content flexible
+
+# Example
+Let's say we don't want the users of some repostiory to update `package-lock.json` with every `npm install`, but we _do_ want to make an easy option `npm run update-package-lock` for consciously updating it.
+
+With PushPull this is easy--just add appropriate comments to make your [.npmrc](./.npmrc) flexible.
+```
+.npmrc
+package-lock=false # UPDATE_OFF
+# UPDATE_ON package-lock=true
+
+package.json/scripts
+{
+  "update-package-lock": "pushpull .npmrc --push '# UPDATE_ON' --pull '# UPDATE_OFF' && npm install && npm dedupe && pushpull .npmrc --push '# UPDATE_OFF' --pull '# UPDATE_ON'"
+}
+```
+
+For this easy case, the `switch` command would be even more appropriate. It does both `push` and `pull` in one run.
+```
+package-lock=false # UPDATE
+# UPDATE package-lock=true
+
+package.json/scripts
+{
+  "update-package-lock": "pushpull .npmrc --switch '# UPDATE' && npm install && npm dedupe && pushpull .npmrc --switch '# UPDATE'"
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,48 +1,45 @@
 const PushPull = (filter, processors) => {
   if (!Array.isArray(processors) || processors.length === 0) {
-    return null;
+    return null
   }
-  this.push = pushRe(push);
-  this.pull = pullRe(pull);
-  return this;
+  this.push = pushRe(push)
+  this.pull = pullRe(pull)
+  return this
 }
 
-
-const filePushPull = (filepath, pushRe, pullRe) => {
-
-}
+const filePushPull = (filepath, pushRe, pullRe) => {}
 
 const linePushPull = (line, pushRe, pullRe) => {
-  let result = line;
+  let result = line
   if (pushRe !== null) {
-    result = result.replace(pushRe, "$1$4$3$2$5");
+    result = result.replace(pushRe, "$1$4$3$2$5")
   }
   if (pullRe !== null) {
-    result = result.replace(pullRe, "$1$4$3$2$5");
+    result = result.replace(pullRe, "$1$4$3$2$5")
   }
-  return result;
-};
+  return result
+}
 
-const strip = input => input.replace(/^\s*(.*?)\s*$/, "$1");
+const strip = input => input.replace(/^\s*(.*?)\s*$/, "$1")
 
-const escapeRegExp = input => input.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+const escapeRegExp = input => input.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
 
 const pushRe = push => {
-  const pushStrippedEscaped = escapeRegExp(strip(push));
-  return pushStrippedEscaped.length > 0 ? RegExp(`^(\\s*)(${pushStrippedEscaped})(\\s*)(.*?)(\\s*)$`, "g") : null;
+  const pushStrippedEscaped = escapeRegExp(strip(push))
+  return pushStrippedEscaped.length > 0 ? RegExp(`^(\\s*)(${pushStrippedEscaped})(\\s*)(.*?)(\\s*)$`, "g") : null
 }
 const pullRe = pull => {
-  const pullStrippedEscaped = escapeRegExp(strip(pull));
-  return pullStrippedEscaped.length > 0 ? RegExp(`^(\\s*)(.*?)(\\s*)(${pullStrippedEscaped})(\\s*)$`, "g") : null;
+  const pullStrippedEscaped = escapeRegExp(strip(pull))
+  return pullStrippedEscaped.length > 0 ? RegExp(`^(\\s*)(.*?)(\\s*)(${pullStrippedEscaped})(\\s*)$`, "g") : null
 }
 
 const testIt = () => {
-  // const line = "   // \HANA_IN       a, bbb";
-  const line = "   a, bbb  // \\HANA_OUT_D";
-  const push = "// HANA_IN";
-  const pull = "// \\HANA_OUT";
+  // const line = "   // \HANA_IN       a, bbb"
+  const line = "   a, bbb  // \\HANA_OUT_D"
+  const push = "// HANA_IN"
+  const pull = "// \\HANA_OUT"
 
-  console.log(`${line}\n${linePushPull(line, pushRe(push), pullRe(pull))}`);
+  console.log(`${line}\n${linePushPull(line, pushRe(push), pullRe(pull))}`)
 }
 
-testIt();
+testIt()
