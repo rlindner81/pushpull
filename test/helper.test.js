c377aa7d38ee62faadbca9d8ba24165c4e880cdd
Richard Lindner <rlindner81@gmail.com>
2019-11-17 11:44:26 +0100

    Full coverage for helper

--- test/helper.test.js
+++ test/helper.test.js
@@ -1,13 +1,24 @@
 const { assert, ordinal, escapeRegExp, noop } = require("../src/helper")
 
+const mockLog = jest.spyOn(console, "error").mockImplementation(noop)
+const mockExit = jest.spyOn(process, "exit").mockImplementation(noop)
+
 test("assert", () => {
-  const mockLog = jest.spyOn(console, "error").mockImplementation(noop)
-  const mockExit = jest.spyOn(process, "exit").mockImplementation(noop)
+  mockLog.mockClear()
+  mockExit.mockClear()
   assert(false, "help")
   expect(mockLog).toBeCalledTimes(1)
   expect(mockLog).toBeCalledWith("error: help")
   expect(mockExit).toBeCalledTimes(1)
   expect(mockExit).toBeCalledWith(-1)
+})
+
+test("!assert", () => {
+  mockLog.mockClear()
+  mockExit.mockClear()
+  assert(true, "help")
+  expect(mockLog).toBeCalledTimes(0)
+  expect(mockExit).toBeCalledTimes(0)
 })
 
 test("ordinal", () => {
